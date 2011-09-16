6158da1abce5b963d19661da8794787085b3db71
Matthew M. Boedicker <matthewm@boedicker.org>
2011-09-15 22:11:58 -0400

    re-add trailing slashes to url paths if PostRank::URI.clean removes them

--- plugins/add_pre_35_url_clean.rb
+++ plugins/add_pre_35_url_clean.rb
@@ -6,24 +6,39 @@ require 'murlsh'
 
 module Murlsh
 
-  # Canonicalize and clean urls with postrank-uri.
-  #
-  # See https://github.com/postrank-labs/postrank-uri
+  # Canonicalize and clean urls.
+
   class AddPre35UrlClean < Plugin
 
     @hook = 'add_pre'
 
     def self.run(url, config)
-      url.url = PostRank::URI.clean(url.url)  if cleanable?(url.url)
-      url.via = PostRank::URI.clean(url.via)  if cleanable?(url.via)
+      Murlsh::failproof { url.url = clean(url.url)  if cleanable?(url.url) }
+      Murlsh::failproof { url.via = clean(url.via)  if cleanable?(url.via) }
     end
 
-    # Return true if the url can be cleaned by postrank-uri.
+    # Canonicalize and clean a url.
     #
-    # Postrank::URI.clean only works on https or https urls.
-    def self.cleanable?(url)
-      Murlsh::failproof { URI(url).scheme.to_s.match(/^https?$/i) }
+    # Call PostRank::URI.clean and re-add a trailing slash to the path if it
+    # was removed. Removing the trailing slash sometimes breaks urls.
+    #
+    # See https://github.com/postrank-labs/postrank-uri
+    def self.clean(url)
+      cleaned = PostRank::URI.clean(url)
+
+      if URI(url).path.end_with? '/'
+        cleaned_uri = URI(cleaned)
+        unless cleaned_uri.path.end_with? '/'
+          cleaned_uri.path << '/'
+          cleaned = cleaned_uri.to_s
+        end
+      end
+
+      cleaned
     end
+
+    # Return true if the url can be cleaned (is http or https).
+    def self.cleanable?(url); URI(url).scheme.to_s.match(/^https?$/i); end
 
   end
 
