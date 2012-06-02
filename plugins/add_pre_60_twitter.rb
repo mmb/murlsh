3bbf3170c2a72e534dc3cfe4375885bd2352e722
Matthew M. Boedicker <matthewm@boedicker.org>
2012-06-01 22:55:52 -0400

    get tweet thumbnail from Twitter API

--- plugins/add_pre_60_twitter.rb
+++ plugins/add_pre_60_twitter.rb
@@ -10,10 +10,28 @@ module Murlsh
     TwitterRe = %r{^https?://twitter\.com/\w+/status(?:es)?/(\d+)$}i
 
     def self.run(url, config)
-      if not url.user_supplied_title? and tweet_id = url.url[TwitterRe, 1]
-        tweet = Twitter.status(tweet_id)
+      if not url.user_supplied_title? or not url.thumbnail_url
+        if tweet_id = url.url[TwitterRe, 1]
+          Murlsh.failproof do
+            tweet = Twitter.status(tweet_id)
 
-        url.title = "@#{tweet.user.screen_name}: #{tweet.text}"
+            if not url.user_supplied_title?
+              url.title = "@#{tweet.user.screen_name}: #{tweet.text}"
+            end
+
+            if not url.thumbnail_url
+              thumb_storage = ImgStore.new(config)
+
+              stored_url = thumb_storage.store_url(
+                tweet.user.profile_image_url) do |i|
+                max_side = config.fetch('thumbnail_max_side', 90)
+                i.extend(ImageList).resize_down!(max_side)
+              end
+
+              url.thumbnail_url = stored_url  if stored_url
+            end
+          end
+        end
       end
     end
 
